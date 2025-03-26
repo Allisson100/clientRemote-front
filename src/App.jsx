fd05f71c9ac8fbe3fbf74f577a0972a415ff56f2
Allisson100 <allisson.boiane@outlook.com>
2025-03-26 18:26:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Client from "./page/Client";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/client/:roomId" element={<Client />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/page/Client/index.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { io } from "socket.io-client";
+
+const socket = io("https://8acc-177-72-141-5.ngrok-free.app", {
+  transports: ["websocket", "polling"], // Garante compatibilidade
+  reconnectionAttempts: 5, // Tenta reconectar até 5 vezes
+  reconnectionDelay: 1000, // Espera 1 segundo entre tentativas
+});
+
+export default function Client() {
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    socket.emit("joinRoom", roomId);
+    socket.on("roomNotFound", () => {
+      alert("Sala não está mais disponível!");
+    });
+  }, [roomId]);
+
+  // Função para capturar o movimento do mouse
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+    socket.emit("moveMouse", { roomId, x: clientX, y: clientY });
+  };
+
+  // Função para capturar o movimento do toque no celular
+  const handleTouchMove = (event) => {
+    const touch = event.touches[0]; // Pega o primeiro toque na tela
+    socket.emit("moveMouse", { roomId, x: touch.clientX, y: touch.clientY });
+  };
+
+  return (
+    <div
+      onMouseMove={handleMouseMove}
+      onTouchMove={handleTouchMove}
+      style={{ width: "100vw", height: "100vh", background: "#eee" }}
+    >
+      <h1>Cliente Conectado na Sala {roomId}</h1>
+      <p>Arraste o dedo na tela para controlar o mouse do Host.</p>
+    </div>
+  );
+}
