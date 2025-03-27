1862dc3cfee1f75acb23e31419c5c14a3cfa54d3
Allisson100 <allisson.boiane@outlook.com>
2025-03-27 19:44:11 -0300

    feat: shareScreen

--- src/page/Client/index.jsx
+++ src/page/Client/index.jsx
@@ -1,43 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
 
-const socket = io("https://b6bc-177-72-141-5.ngrok-free.app", {
-  transports: ["websocket", "polling"], // Garante compatibilidade
-  reconnectionAttempts: 5, // Tenta reconectar até 5 vezes
-  reconnectionDelay: 1000, // Espera 1 segundo entre tentativas
+const socket = io("https://42f2-177-72-141-5.ngrok-free.app", {
+  transports: ["websocket", "polling"],
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
 });
 
 export default function Client() {
   const { roomId } = useParams();
+  const videoRef = useRef(null);
 
   useEffect(() => {
     socket.emit("joinRoom", roomId);
     socket.on("roomNotFound", () => {
       alert("Sala não está mais disponível!");
     });
+
+    // Recebendo a transmissão da tela
+    socket.on("screenShareSignal", (track) => {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(track);
+      videoRef.current.srcObject = mediaStream;
+    });
+
+    // Parar a exibição caso o host desligue a tela
+    socket.on("screenShareStopped", () => {
+      if (videoRef.current.srcObject) {
+        videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
+        videoRef.current.srcObject = null;
+      }
+    });
   }, [roomId]);
 
-  // Função para capturar o movimento do mouse
-  const handleMouseMove = (event) => {
-    const { clientX, clientY } = event;
-    socket.emit("moveMouse", { roomId, x: clientX, y: clientY });
-  };
-
-  // Função para capturar o movimento do toque no celular
-  const handleTouchMove = (event) => {
-    const touch = event.touches[0]; // Pega o primeiro toque na tela
-    socket.emit("moveMouse", { roomId, x: touch.clientX, y: touch.clientY });
-  };
-
   return (
-    <div
-      onMouseMove={handleMouseMove}
-      onTouchMove={handleTouchMove}
-      style={{ width: "100vw", height: "100vh", background: "#eee" }}
-    >
+    <div style={{ textAlign: "center" }}>
       <h1>Cliente Conectado na Sala {roomId}</h1>
       <p>Arraste o dedo na tela para controlar o mouse do Host.</p>
+
+      <video
+        ref={videoRef}
+        autoPlay
+        style={{ width: "90vw", border: "2px solid black" }}
+      ></video>
     </div>
   );
 }
